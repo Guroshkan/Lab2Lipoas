939937788081ad3122704157253d416880a9d286
Nikol <44338413+Guroshkan@users.noreply.github.com>
2021-03-21 14:51:04 +0300

    Add files via upload

--- Lab2Lipoas/Lab2Lipoas/File.h
+++ Lab2Lipoas/Lab2Lipoas/File.h
@@ -6,6 +6,8 @@
 #include <fstream>
 #include <filesystem>
 #include "Console.h"
+#include "rule.h"
+#include "logic.h"
 enum userYoN { Yes = 1, No };
 enum ModOpenFile { Write = true, Read = false };
 namespace fs = std::filesystem;
@@ -14,6 +16,8 @@ class File
 public:
 	static void saveMatrix(int, int, int, int, grid);
 	static grid loadMatrix();
+	static void saveRules(rule);
+	static rule loadRules();
 	static std::vector<std::string> split(std::string,char);
 	static std::string getMessage(std::string);
 	static std::string getCorrectPath(bool);
